cea3c18bae0dd330936244b70589d7a4d4e36fec
FrancoGaggero <franco.gaggero@davinci.edu.ar>
2024-09-09 10:43:35 -0300

    inicio proyecto

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,26 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		///GESTION DE TRABAJADORES
 		LocalDate hoy = LocalDate.now();
-		JOptionPane.showMessageDialog(null, hoy);
-		String nombre= JOptionPane.showInputDialog("Ingrese tu nombre");
-		String a ="";
-		String b = "";
-		String c = "";
+		String opciones []= {
+			"Vacaiones",""	
+		};
+		String nombre = validar(" ingrese su nombre ");
 	
 		
 		
+	}
+	
+	//////FUNCIONES
+	public static String validar(String mensaje) {
+		String a = JOptionPane.showInputDialog(mensaje);
+		while (a.isEmpty()) {
+			a = JOptionPane.showInputDialog("Error: " + mensaje);
+		}
+
+		return a;
 	}
 
 }
---
+++ src/Trabajador.java
@@ -0,0 +1,11 @@
+
+public class Trabajador {
+
+	//ATRIBUTOS
+	
+	
+	
+	
+	
+	
+}
