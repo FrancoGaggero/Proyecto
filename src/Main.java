29a647cda8bc3f55e0e0786bf12a03751b4a89ee
FrancoGaggero <franco.gaggero@davinci.edu.ar>
2024-09-09 10:11:13 -0300

    agrego fecha

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import java.time.LocalDate;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		LocalDate hoy = LocalDate.now();
+		JOptionPane.showMessageDialog(null, hoy);
 		
 		
 	}
