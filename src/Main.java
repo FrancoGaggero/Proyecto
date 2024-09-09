e25b98afbf8fd07f4858daa00ecb4e1b6f0a0a93
FrancoGaggero <franco.gaggero@davinci.edu.ar>
2024-09-09 10:09:20 -0300

    creacion del repositorio

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalDate hoy = LocalDate.now();
+		
+		
+	}
+
+}
